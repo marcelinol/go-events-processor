18e6b28b2619f77f7fd5948397dc51ef1b87ca77
marcelinol <marcelinolucianom@gmail.com>
2017-11-01 20:26:30 -0200

    Read lines from each file of the folder

--- processor.go
+++ processor.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"time"
 )
 
-func doEvery(d time.Duration, f func(time.Time)) {
-	for x := range time.Tick(d) {
-		f(x)
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
 
-func helloworld(t time.Time) {
-	fmt.Printf("%v: Hello, World!\n", t)
+func readFile() {
+	files, err := ioutil.ReadDir("./tmp")
+	check(err)
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+		file, err := os.Open("./tmp/" + file.Name())
+		check(err)
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func doEvery(d time.Duration, f func()) {
+	count := 0
+	for range time.Tick(d) {
+		f()
+		count++
+		if count > 0 {
+			return
+		}
+	}
+}
+
+func helloworld() {
+	fmt.Printf("Hello, World!\n")
 }
 
 func main() {
-	doEvery(20*time.Millisecond, helloworld)
+	doEvery(2000*time.Millisecond, helloworld)
+	doEvery(5000*time.Millisecond, readFile)
 }
